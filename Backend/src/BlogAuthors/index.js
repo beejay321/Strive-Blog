33c06b866f9babcd7e3524d4a1713ddae340beb2
beejay321 <jibodu.busola@gmail.com>
2021-05-19 04:01:44 +0200

    Update

--- Backend/src/BlogAuthors/index.js
+++ Backend/src/BlogAuthors/index.js
@@ -1,25 +1,33 @@
-import express from "express";
-import fs from "fs";
+import express from "express"; // third party module(needs to ne installed)
+import fs from "fs"; // core module (does not need to be installed)
 import path from "path";
-// import uniqid from "uniqid";
+import uniqid from "uniqid";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 import { parse } from "node:path";
+import { url } from "inspector";
 
 const authorsRouter = express.Router();
 
 const filePath = fileURLToPath(import.meta.url);
 const authorsFolderPath = dirname(filePath);
-const authorsJSONPath = join(authorsFolderPath, "./authors.json");
+const authorsJSONPath = join(authorsFolderPath, "authors.json");
+console.log(url);
 
 authorsRouter.get("/", (req, res) => {
   const contentAsBuffer = fs.readFileSync(authorsJSONPath);
+  console.log(contentAsBuffer);
   const contentAsString = contentAsBuffer.toString();
+  console.log(contentAsString);
   const authors = JSON.parse(contentAsString);
   res.send(authors);
+  // res.send({
+  //   filePath,
+  //   authorsFolderPath,
+  //   url: import.meta.url,
+  //   authorsJSONPath,
+  // });
 });
-
-
 
 authorsRouter.get("/:id", (req, res) => {
   const contentAsBuffer = fs.readFileSync(authorsJSONPath);
@@ -33,9 +41,13 @@ authorsRouter.get("/:id", (req, res) => {
   // res.send(authors);
 });
 
-
-
 authorsRouter.post("/", (req, res) => {
+  console.log(req.url);
+  console.log(req.headers);
+  res.send("I am the post");
+  console.log(req.body);
+
+  // const newAuthor = req.body;
   const newAuthor = { ...req.body, createdAt: new Date(), _id: uniqid() };
 
   console.log(newAuthor);
@@ -44,13 +56,11 @@ authorsRouter.post("/", (req, res) => {
 
   authors.push(newAuthor);
 
+  console.log(authors);
   fs.writeFileSync(authorsJSONPath, JSON.stringify(authors));
 
   res.status(201).send(newAuthor._id);
 });
-
-
-
 
 authorsRouter.put("/:id", (req, res) => {
   const authors = JSON.parse(fs.readFileSync(authorsJSONPath).toString());
@@ -74,6 +84,7 @@ authorsRouter.delete("/:id", (req, res) => {
     (author) => author._id !== req.params.id
   );
   fs.writeFileSync(authorsJSONPath, JSON.stringify(remainingAuthors));
+  // res.send("I am the delete request");
 
   res.status(204).send();
 });
