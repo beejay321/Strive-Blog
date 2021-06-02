4a835378c4670578f5ac8646b6555cab13bad4a6
beejay321 <jibodu.busola@gmail.com>
2021-06-02 16:59:49 +0200

    Updated comments routes

--- src/BlogPosts/index.js
+++ src/BlogPosts/index.js
@@ -196,22 +196,6 @@ BlogPostsRouter.post("/:id", async (req, res, next) => {
 
 BlogPostsRouter.get("/:id/comments", async (req, res, next) => {
   try {
-    const commentsById = await blogPostsModel.findById(req.params.id, {comments: 1,});
-    if (commentsById) {
-      res.send(commentsById);
-    } else {
-      next(createError(404, `Post ${req.params.id} has no comments `));
-      // createError(err.status, error.message)
-    }
-  } catch (error) {
-    next(error);
-  }
-});
-
-/****************GET SINGLE COMMENT FOR A SPECIFIED POST******************/
-
-BlogPostsRouter.get("/:id/comments/commentId", async (req, res, next) => {
-  try {
     const commentsById = await blogPostsModel.findById(req.params.id, {
       comments: 1,
     });
@@ -226,10 +210,50 @@ BlogPostsRouter.get("/:id/comments/commentId", async (req, res, next) => {
   }
 });
 
+/****************GET SINGLE COMMENT FOR A SPECIFIED POST******************/
+
+BlogPostsRouter.get("/:id/comments/:commentId", async (req, res, next) => {
+  try {
+    const singlecommentById = await blogPostsModel.findOne(
+      { _id: req.params.id },
+      {
+        comments: {
+          $elemMatch: { _id: req.params.commentId },
+        },
+      }
+    );
+    if (singlecommentById) {
+      res.send(singlecommentById);
+    } else {
+      next(createError(404, `comment ${req.params.comentId} not found `));
+      // createError(err.status, error.message)
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
 /****************UPDATE COMMENT FOR A SPECIFIED POST******************/
 
 BlogPostsRouter.put("/:id/comments/:commentId", async (req, res, next) => {
   try {
+    const singlecommentById = await blogPostsModel.findOneAndUpdate(
+    {
+      _id: req.params.id,
+      "comments._id": req.params.commentId,
+    }, 
+    {$set: {"comments.$": req.body}
+    },
+    {
+      runValidators: true,
+        new: true,
+    }
+    );
+    if (singlecommentById) {
+      res.send(singlecommentById)
+    } else {
+      next(createError(404, `comment ${req.params.commentId} not found`))
+    }
   } catch (error) {
     next(error);
   }
@@ -239,7 +263,7 @@ BlogPostsRouter.put("/:id/comments/:commentId", async (req, res, next) => {
 
 BlogPostsRouter.delete("/:id/comments/:commentId", async (req, res, next) => {
   try {
-    const modifiedPost = await blogPostsModel.findByIdAndUpdate(
+    await blogPostsModel.findByIdAndUpdate(
       req.params.id,
       { $pull: { comments: { _id: req.params.commentId } } },
       { new: true }
